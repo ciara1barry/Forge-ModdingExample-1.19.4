a6d7eef3c68415a28e177c2b1420d09eda06fc76
ciara1barry <ciara2barry@gmail.com>
2023-04-23 11:16:07 +0100

    Testing Commit with IntelliJ

--- src/main/java/net/cbar/examplemod/ExampleMod.java
+++ src/main/java/net/cbar/examplemod/ExampleMod.java
@@ -27,11 +27,10 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "examplemod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    // Test
     public ExampleMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
