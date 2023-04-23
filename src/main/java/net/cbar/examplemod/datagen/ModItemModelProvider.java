701e8d854d205c42a777d8b7822183129107e649
ciara1barry <ciara2barry@gmail.com>
2023-04-24 00:22:25 +0100

    Created Classes for semi-auto JSON generation

---
+++ src/main/java/net/cbar/examplemod/datagen/ModItemModelProvider.java
@@ -0,0 +1,35 @@
+package net.cbar.examplemod.datagen;
+
+import net.cbar.examplemod.ExampleMod;
+import net.cbar.examplemod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, ExampleMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.BLACK_OPAL);
+        simpleItem(ModItems.RAW_BLACK_OPAL);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(ExampleMod.MOD_ID, "item/" + item.getId().getPath()));
+    }
+
+    private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/handheldd")).texture("layer0",
+                new ResourceLocation(ExampleMod.MOD_ID, "item/" + item.getId().getPath()));
+    }
+}
