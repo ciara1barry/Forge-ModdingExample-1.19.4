e41d273c28d1304f98e1c0cdeeb6983ef00fca68
ciara1barry <ciara2barry@gmail.com>
2023-04-24 02:18:23 +0100

    Custom Tree now generates in the world

--- src/main/java/net/cbar/examplemod/datagen/ModWorldGenProvider.java
+++ src/main/java/net/cbar/examplemod/datagen/ModWorldGenProvider.java
@@ -2,6 +2,7 @@ package net.cbar.examplemod.datagen;
 
 import net.cbar.examplemod.ExampleMod;
 import net.cbar.examplemod.worldgen.ModConfiguredFeatures;
+import net.cbar.examplemod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -13,7 +14,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(ExampleMod.MOD_ID));
